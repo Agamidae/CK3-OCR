992e9fa6b33b40bd6915b2b61df3bdedb7a96ce6
Agamidae <agamidaex@gmail.com>
2023-08-17 11:25:11 +0200

    copied locs

---
+++ copy to other languages.bat
@@ -0,0 +1,3 @@
+cd dist
+copy_on_other_languages.exe "D:\Documents\Paradox Interactive\Crusader Kings III\mod\OCR-Support\OCR-Support\localization" english french german korean russian simp_chinese spanish
+pause
