444f069c2436143e51272c6ea5493959e5296f38
Agamidae <agamidaex@gmail.com>
2023-12-26 10:57:10 +0200

    removed fluff

--- copy to other languages.bat
+++
@@ -1,3 +0,0 @@
-cd dist
-copy_on_other_languages.exe "D:\Documents\Paradox Interactive\Crusader Kings III\mod\OCR-Support\OCR-Support\localization" english french german korean russian simp_chinese spanish
-pause
